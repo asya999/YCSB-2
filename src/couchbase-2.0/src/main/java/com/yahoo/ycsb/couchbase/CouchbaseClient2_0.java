728037030cbd055d7f8020a0f64e8362e1fbb5ef
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-06 18:16:17 +0300

    get rid of commented code related to scan implementation

--- src/couchbase-2.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient2_0.java
+++ src/couchbase-2.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient2_0.java
@@ -10,7 +10,6 @@ import com.yahoo.ycsb.workloads.RangeScanOperation;
 import net.spy.memcached.MemcachedClient;
 
 import java.net.URI;
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,14 +17,6 @@ import java.util.Set;
 
 @SuppressWarnings({"NullableProblems"})
 public class CouchbaseClient2_0 extends MemcachedCompatibleClient implements RangeScanOperation {
-
-    private static final SecureRandom RANDOM = new SecureRandom();
-
-    public static final String SCAN_DESIGN_DOCUMENT_NAME = "";
-
-    public static final String SCAN_VIEW_NAME = "_all_docs";
-
-//    protected View scanView;
 
     private CouchbaseConfig couchbaseConfig;
 
@@ -57,54 +48,11 @@ public class CouchbaseClient2_0 extends MemcachedCompatibleClient implements Ran
 
     @Override
     public int scan(String table, String startKey, int limit, Set<String> fields, List<Map<String, ByteIterator>> result) {
-//        try {
-//            Query query = new Query();
-//            query.setIncludeDocs(true);
-//            query.setRangeStart(createQualifiedKey(table, startKey));
-//            query.setLimit(limit);
-//            scanQuery(query, result);
-//            return OK;
-//        } catch (Exception e) {
-//            if (log.isErrorEnabled()) {
-//                log.error("Error performing scan starting with a key: " + startKey, e);
-//            }
-//            return ERROR;
-//        }
         throw new UnsupportedOperationException("Scan not implemented");
     }
 
     @Override
     public int scan(String table, String startKey, String endKey, int limit, Set<String> fields, List<Map<String, ByteIterator>> result) {
-//        try {
-//            Query query = new Query();
-//            query.setIncludeDocs(true);
-//            query.setRangeStart(createQualifiedKey(table, startKey));
-//            query.setRangeEnd(createQualifiedKey(table, endKey));
-//            query.setLimit(limit);
-//            scanQuery(query, result);
-//            return OK;
-//        } catch (Exception e) {
-//            if (log.isErrorEnabled()) {
-//                log.error("Error performing scan starting with a key: " + startKey, e);
-//            }
-//            return ERROR;
-//        }
         throw new UnsupportedOperationException("Scan not implemented");
     }
-
-//    protected void scanQuery(Query query, List<Map<String, ByteIterator>> result) throws IOException {
-//        ViewResponse response = ((CouchbaseClient) client).query(createScanView(), query);
-//        for (ViewRow row : response) {
-//            Map<String, ByteIterator> documentAsMap = new HashMap<String, ByteIterator>();
-//            fromJson((String) row.getDocument(), null, documentAsMap);
-//            result.add(documentAsMap);
-//        }
-//    }
-//
-//    protected View createScanView() {
-//        if (scanView == null) {
-//            scanView = ((CouchbaseClient) client).getView(SCAN_DESIGN_DOCUMENT_NAME, SCAN_VIEW_NAME);
-//        }
-//        return scanView;
-//    }
 }
