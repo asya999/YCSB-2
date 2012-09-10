29103457773afd5bf0bd66a8508a4c11cd65a113
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-10 09:28:29 -0700

    use CouchbaseClient for all operations

--- src/couchbase-2.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient2_0.java
+++ src/couchbase-2.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient2_0.java
@@ -6,43 +6,66 @@ import com.couchbase.client.protocol.views.Query;
 import com.couchbase.client.protocol.views.ViewResponse;
 import com.couchbase.client.CouchbaseConnectionFactory;
 import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
+import com.yahoo.ycsb.ByteIterator;
+import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.memcached.MemcachedCompatibleClient;
-import com.yahoo.ycsb.memcached.MemcachedCompatibleConfig;
-import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.internal.GetFuture;
+import net.spy.memcached.internal.OperationFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collection;
+import java.util.*;
 
 @SuppressWarnings({"NullableProblems"})
 public class CouchbaseClient2_0 extends MemcachedCompatibleClient {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     protected CouchbaseClient client;
 
-    private CouchbaseConfig couchbaseConfig;
+    protected CouchbaseConfig couchbaseConfig;
 
     private static String DDOC_NAME = "ddoc";
+
     private static String VIEW_NAME = "view";
 
-    @Override
-    protected MemcachedCompatibleConfig createMemcachedConfig() {
-        return couchbaseConfig = new CouchbaseConfig(getProperties());
-    }
+    private static View view;
+
+    private boolean checkOperationStatus;
+
+    private long shutdownTimeoutMillis;
+
+    private int objectExpirationTime;
 
     @Override
-    protected MemcachedClient createMemcachedClient() throws Exception {
+    public void init() throws DBException {
+        try {
+            couchbaseConfig = createMemcachedConfig();
+            client = createMemcachedClient();
+            checkOperationStatus = couchbaseConfig.getCheckOperationStatus();
+            objectExpirationTime = couchbaseConfig.getObjectExpirationTime();
+            shutdownTimeoutMillis = couchbaseConfig.getShutdownTimeoutMillis();
+        } catch (Exception e) {
+            throw new DBException(e);
+        }
+    }
+
+    protected CouchbaseConfig createMemcachedConfig() {
+        return new CouchbaseConfig(getProperties());
+    }
+
+    protected CouchbaseClient createMemcachedClient() throws Exception {
         return createCouchbaseClient();
     }
 
     protected CouchbaseClient createCouchbaseClient() throws Exception {
         CouchbaseConnectionFactoryBuilder builder = new CouchbaseConnectionFactoryBuilder();
-        builder.setReadBufferSize(config.getReadBufferSize());
-        builder.setOpTimeout(config.getOpTimeout());
-        builder.setFailureMode(config.getFailureMode());
+        builder.setReadBufferSize(couchbaseConfig.getReadBufferSize());
+        builder.setOpTimeout(couchbaseConfig.getOpTimeout());
+        builder.setFailureMode(couchbaseConfig.getFailureMode());
 
         List<URI> servers = new ArrayList<URI>();
-        for (String address : config.getHosts().split(",")) {
+        for (String address : couchbaseConfig.getHosts().split(",")) {
             servers.add(new URI("http://" + address + ":8091/pools"));
         }
         CouchbaseConnectionFactory connectionFactory =
@@ -52,13 +75,68 @@ public class CouchbaseClient2_0 extends MemcachedCompatibleClient {
     }
 
     @Override
+    public int read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+        try {
+            GetFuture<Object> future = client.asyncGet(createQualifiedKey(table, key));
+            Object document = future.get();
+            if (document != null) {
+                fromJson((String) document, fields, result);
+            }
+            return OK;
+        } catch (Exception e) {
+            if (log.isErrorEnabled()) {
+                log.error("Error encountered", e);
+            }
+            return ERROR;
+        }
+    }
+
+    @Override
+    public int update(String table, String key, Map<String, ByteIterator> values) {
+        key = createQualifiedKey(table, key);
+        try {
+            OperationFuture<Boolean> future = client.replace(key, objectExpirationTime, toJson(values));
+            return getReturnCode(future);
+        } catch (Exception e) {
+            if (log.isErrorEnabled()) {
+                log.error("Error updating value with key: " + key, e);
+            }
+            return ERROR;
+        }
+    }
+
+    @Override
+    public int insert(String table, String key, Map<String, ByteIterator> values) {
+        key = createQualifiedKey(table, key);
+        try {
+            OperationFuture<Boolean> future = client.add(key, objectExpirationTime, toJson(values));
+            return getReturnCode(future);
+        } catch (Exception e) {
+            if (log.isErrorEnabled()) {
+                log.error("Error inserting value", e);
+            }
+            return ERROR;
+        }
+    }
+
+    @Override
+    public int delete(String table, String key) {
+        key = createQualifiedKey(table, key);
+        try {
+            OperationFuture<Boolean> future = client.delete(key);
+            return getReturnCode(future);
+        } catch (Exception e) {
+            if (log.isErrorEnabled()) {
+                log.error("Error deleting value", e);
+            }
+            return ERROR;
+        }
+    }
+
+    @Override
     public int query(String table, String key, int limit) {
         key = createQualifiedKey(table, key);
-
-        Query query = new Query();
-        query.setKey(key);
-        query.setLimit(limit);
-        ViewResponse response = client.query(get_view(), query);
+        ViewResponse response = client.query(get_view(), get_query(key, limit));
 
         Collection errors = response.getErrors();
         if (errors.isEmpty() == true) {
@@ -69,7 +147,16 @@ public class CouchbaseClient2_0 extends MemcachedCompatibleClient {
     };
 
     private View get_view() {
-        View view = client.getView(DDOC_NAME, VIEW_NAME);
+        if (view == null) {
+            view = client.getView(DDOC_NAME, VIEW_NAME);
+        }
         return  view;
+    }
+
+    private Query get_query(String key, int limit) {
+        Query query = new Query();
+        //query.setKey(key);
+        query.setLimit(limit);
+        return query;
     }
 }
