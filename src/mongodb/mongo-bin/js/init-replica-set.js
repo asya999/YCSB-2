7df75c9907b67e30778268282e3e27ad83ae6e01
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-20 15:05:36 -0700

    fix replica configuration script

--- src/mongodb/mongo-bin/js/init-replica-set.js
+++ src/mongodb/mongo-bin/js/init-replica-set.js
@@ -1,25 +1,15 @@
-var replicaSet = "shard1";
-var members = ["ycsb-node1:27017", "ycsb-node2:27018"];
-var replicaSet = "shard2";
-var members = ["ycsb-node2:27017", "ycsb-node1:27018"];
-var replicaSet = "shard3";
-var members = ["ycsb-node3:27017", "ycsb-node4:27018"];
-var replicaSet = "shard4";
-var members = ["ycsb-node4:27017", "ycsb-node3:27018"];
-
-var replicaSetConfig = {
-    _id : replicaSet,
-    members : []
+var config = {
+    _id : "rs0",
+    members : [
+        {_id : 0, host : "ycsb-node1"},
+        {_id : 1, host : "ycsb-node2"},
+        {_id : 2, host : "ycsb-node3"},
+        {_id : 3, host : "ycsb-node4"},
+    ]
 };
 
-for (var i = 0; i < members.length; i++) {
-    replicaSetConfig.members.push({_id : i, host : members[i]});
-}
+rs.initiate(config);
 
-rs.initiate(replicaSetConfig);
-
-// By default, clients will direct reads to the primary node in a cluster. To distribute reads to secondary nodes, most
-// drivers allow you to configure read preference on a per-connection or per-operation basis. To enable secondary reads:
 rs.slaveOk();
 
 rs.status();
