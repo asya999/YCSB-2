0b0976c7d9c7c31f956f9fc51d79e77ea43c10f2
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-20 15:26:26 -0700

    clean everything before new mongodb installation

--- src/bin/install-mongo-local.sh
+++ src/bin/install-mongo-local.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 MONGO_VERSION=2.2.0
 
+# Clean up
+killall -9 mongos mongod
+rm -fr /usr/lib/mongodb
+rm -fr /var/log/mongodb
+rm -fr /data
+
 wget http://downloads.mongodb.org/linux/mongodb-linux-x86_64-${MONGO_VERSION}.tgz -P /tmp
 tar xzf /tmp/mongodb-linux-x86_64-${MONGO_VERSION}.tgz -C /tmp
 mv /tmp/mongodb-linux-x86_64-${MONGO_VERSION}/ /usr/lib/mongodb
 rm /tmp/mongodb-linux-x86_64-${MONGO_VERSION}.tgz
 
-killall -9 mongos mongod
 mongo-bin/start-mongod.sh -r rs0
 mongo-bin/start-mongod-configsvr.sh
 mongo-bin/start-mongos.sh
