7b410135c04bc97dc6900f15b48123ad22c6ca5d
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-20 18:50:44 -0700

    add quiet flag to replica scriptOF

--- src/mongodb/mongo-bin/init-replica.sh
+++ src/mongodb/mongo-bin/init-replica.sh
@@ -6,11 +6,11 @@ hostname=`hostname`
 
 case $hostname in
     ycsb-node1 )
-        $MONGO_HOME/bin/mongo $BASE_DIR/js/init-replica-set-rs1.js ;;
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs1.js ;;
     ycsb-node2 )
-        $MONGO_HOME/bin/mongo $BASE_DIR/js/init-replica-set-rs2.js ;;
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs2.js ;;
     ycsb-node2 )
-        $MONGO_HOME/bin/mongo $BASE_DIR/js/init-replica-set-rs3.js ;;
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs3.js ;;
     ycsb-node2 )
-        $MONGO_HOME/bin/mongo $BASE_DIR/js/init-replica-set-rs4.js ;;
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs4.js ;;
 esac
