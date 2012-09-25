1215961c8f833aeb30544daabef1fd8209b0c7c3
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-25 12:21:00 -0700

    add script for building initial index

---
+++ src/mongodb/mongo-bin/init-index.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+BASE_DIR=`dirname $0`
+. $BASE_DIR/mongo-env.sh
+
+hostname=`hostname`
+
+case $hostname in
+    ycsb-node1 )
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-index.js ;;
+    ycsb-node2 )
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-index.js ;;
+    ycsb-node3 )
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-index.js ;;
+    ycsb-node4 )
+        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-index.js ;;
+esac
