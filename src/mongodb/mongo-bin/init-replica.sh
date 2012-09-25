c6e237daafe72b4c455931db2d543d396d97aff2
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-25 15:03:12 -0700

    simplify init script

--- src/mongodb/mongo-bin/init-replica.sh
+++ src/mongodb/mongo-bin/init-replica.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-BASE_DIR=`dirname $0`
-. $BASE_DIR/mongo-env.sh
+. mongo-env.sh
 
 hostname=`hostname`
 
 case $hostname in
     ycsb-node1 )
-        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs1.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-rs1.js ;;
     ycsb-node2 )
-        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs2.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-rs2.js ;;
     ycsb-node3 )
-        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs3.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-rs3.js ;;
     ycsb-node4 )
-        $MONGO_HOME/bin/mongo --quiet $BASE_DIR/js/init-replica-set-rs4.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-rs4.js ;;
 esac
