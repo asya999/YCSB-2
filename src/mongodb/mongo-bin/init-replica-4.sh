4340a45e5d225b2f71969614bc5e122e9dfc48e5
ronniedada <ronniedada@gmail.com>
2013-03-22 14:09:14 -0700

    use mongod port for replica init

--- src/mongodb/mongo-bin/init-replica-4.sh
+++ src/mongodb/mongo-bin/init-replica-4.sh
@@ -5,11 +5,11 @@ hostname=`hostname`
 
 case $hostname in
     ycsb-node1 )
-        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs1.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs1.js --port 27018;;
     ycsb-node2 )
-        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs2.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs2.js --port 27018;;
     ycsb-node3 )
-        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs3.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs3.js --port 27018;;
     ycsb-node4 )
-        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs4.js ;;
+        $MONGO_HOME/bin/mongo --quiet js/init-replica-set-4-rs4.js --port 27018;;
 esac
