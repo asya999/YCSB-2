8226e41a8e831c088ed224a1be5eb9d28ab2eae5
ronniedada <ronniedada@gmail.com>
2013-03-22 16:49:21 -0700

    line ending in install scripts

--- src/bin/install-mongo-local-4.sh
+++ src/bin/install-mongo-local-4.sh
@@ -12,16 +12,16 @@ case $hostname in
     ycsb-node1 )
         mongo-bin/start-mongod.sh -r rs1 -p 27018 -d /data
         mongo-bin/start-mongod.sh -r rs4 -p 27019 -d /data1
-        mongo-bin/start-mongod-configsvr.sh -p 27017
+        mongo-bin/start-mongod-configsvr.sh -p 27017 ;;
     ycsb-node2 )
         mongo-bin/start-mongod.sh -r rs2 -p 27018 -d /data
         mongo-bin/start-mongod.sh -r rs1 -p 27019 -d /data1
-        mongo-bin/start-mongod-configsvr.sh -p 27017
+        mongo-bin/start-mongod-configsvr.sh -p 27017 ;;
     ycsb-node3 )
         mongo-bin/start-mongod.sh -r rs3 -p 27018 -d /data
         mongo-bin/start-mongod.sh -r rs2 -p 27019 -d /data1
-        mongo-bin/start-mongod-configsvr.sh -p 27017
+        mongo-bin/start-mongod-configsvr.sh -p 27017 ;;
     ycsb-node4 )
         mongo-bin/start-mongod.sh -r rs4 -p 27018 -d /data
-        mongo-bin/start-mongod.sh -r rs3 -p 27019 -d /data1
+        mongo-bin/start-mongod.sh -r rs3 -p 27019 -d /data1 ;;
 esac
